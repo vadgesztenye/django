2532ca8b6bd484ef8b4830cd7ebe3697b1114861
vadgesztenye <eniko.kollar@yahoo.com>
2016-07-13 19:52:19 +0200

    posts sorted by date

--- project/starter_app/views.py
+++ project/starter_app/views.py
@@ -1,24 +1,31 @@
 import json
-
+import os
 from django.shortcuts import render
 # from .models import Message
-
 from config.settings.base import DJANGO_ROOT
 #import django root from the libraries after manage.py
-import os
+from dateutil import parser
+#datetime parser (elemző)
+
 
 
 def load_data():
 	json_file_path = os.path.join(DJANGO_ROOT,'data','blogs.json')
+	
 	with open(json_file_path) as raw_file:
 		blog_data = json.load(raw_file)
-	return blog_data
+	
+	for post_data in blog_data:
+		post_data['date'] = parser.parse(post_data['date'])	
+
+	sorted_data = sorted(blog_data, key=lambda k:k['date'], reverse=True)
+	
+	return sorted_data
 
 def get_post_by_slug(slug):
 	for post in blog_data:
 		if post['slug'] == slug:
 			return post
-
 
 blog_data = load_data()
 
